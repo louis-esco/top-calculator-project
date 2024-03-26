bbb24de1cbc54fb40d02a7a2302704508615a28e
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-26 16:06:37 +0100

    Adds queryselectors for buttons and display

--- app.js
+++ app.js
@@ -30,4 +30,26 @@ function operate(operator, a, b) {
 // Variables
 let firstInput = 0;
 let secondInput = 0;
-let operator = "";
+let operator = ["", false];
+
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+const equal = document.querySelector('#equal');
+
+const clear = document.querySelector('#clear');
+
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+const dot = document.querySelector('#dot');
+
+const display = document.querySelector('.screen');
