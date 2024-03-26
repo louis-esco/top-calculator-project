7786be65f24e72b81f2ae8fb0a484ff11ef54618
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-03-26 16:19:37 +0100

    Displays digits when clicked

--- app.js
+++ app.js
@@ -32,24 +32,63 @@ let firstInput = 0;
 let secondInput = 0;
 let operator = ["", false];
 
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const multiply = document.querySelector('#multiply');
-const divide = document.querySelector('#divide');
-const equal = document.querySelector('#equal');
+const addBtn = document.querySelector('#add');
+const subtractBtn = document.querySelector('#subtract');
+const multiplyBtn = document.querySelector('#multiply');
+const divideBtn = document.querySelector('#divide');
+const equalBtn = document.querySelector('#equal');
 
-const clear = document.querySelector('#clear');
+const clearBtn = document.querySelector('#clear');
 
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-const dot = document.querySelector('#dot');
+const oneBtn = document.querySelector('#one');
+const twoBtn = document.querySelector('#two');
+const threeBtn = document.querySelector('#three');
+const fourBtn = document.querySelector('#four');
+const fiveBtn = document.querySelector('#five');
+const sixBtn = document.querySelector('#six');
+const sevenBtn = document.querySelector('#seven');
+const eightBtn = document.querySelector('#eight');
+const nineBtn = document.querySelector('#nine');
+const zeroBtn = document.querySelector('#zero');
+const dotBtn = document.querySelector('#dot');
 
 const display = document.querySelector('.screen');
+
+function displayDigit(num) {
+    if (operator[1] === true || display.textContent === '0') {
+        display.textContent = `${num}`;
+    } else {
+        display.textContent += `${num}`;
+    }
+}
+
+oneBtn.addEventListener('click', () => {
+    displayDigit(1);
+})
+twoBtn.addEventListener('click', () => {
+    displayDigit(2);
+})
+threeBtn.addEventListener('click', () => {
+    displayDigit(3);
+})
+fourBtn.addEventListener('click', () => {
+    displayDigit(4);
+})
+fiveBtn.addEventListener('click', () => {
+    displayDigit(5);
+})
+sixBtn.addEventListener('click', () => {
+    displayDigit(6);
+})
+sevenBtn.addEventListener('click', () => {
+    displayDigit(7);
+})
+eightBtn.addEventListener('click', () => {
+    displayDigit(8);
+})
+nineBtn.addEventListener('click', () => {
+    displayDigit(9);
+})
+zeroBtn.addEventListener('click', () => {
+    displayDigit(0);
+})
